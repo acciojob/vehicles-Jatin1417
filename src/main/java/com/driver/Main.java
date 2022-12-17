fa8c3de0d01d41fb667a9973119078df640d3198
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-17 15:44:31 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,19 +6,7 @@ public class Main {
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
-        Main m = new Main();
-        WaterVehicle v = m.new Boat();
-        v.getVehicleName();
-        v.getVehicleCapacity();
+       
     }
-    public class Boat implements WaterVehicle{
-        String name;
-        int capacity;
-        public String getVehicleName(){
-            return name;
-        }
-        public int getVehicleCapacity(){
-           return capacity;
-        }
-    }
+   
 }
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,5 +4,3 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-
-  
