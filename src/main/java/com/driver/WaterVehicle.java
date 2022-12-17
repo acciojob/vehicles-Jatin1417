63bd5caa98c307c093b8941a6b5299aba4dd2a1c
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-17 16:04:19 +0530

    second commit

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -3,4 +3,18 @@ package com.driver;
 public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
+    public class Boat implements WaterVehicle{
+        String name;
+        int capacity;
+        Boat(String name,int capacity){
+            this.name = name;
+            this.capacity = capacity;
+        }
+        public String getVehicleName(){
+            return this.name;
+        }
+        public int getVehicleCapacity(){
+           return this.capacity;
+        }
+    }
 }
